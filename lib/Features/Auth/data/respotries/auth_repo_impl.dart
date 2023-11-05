9ff131b565f9c692c6609d8b90433af2cd66121a
ranaalkoud0 <ranaalkoud0@gmail.com>
2023-11-05 20:33:15 +0200

    sign in with google

--- lib/Features/Auth/data/respotries/auth_repo_impl.dart
+++ lib/Features/Auth/data/respotries/auth_repo_impl.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:vegetables_shop/Features/Auth/domain/respotries/auth_repo.dart';
 
 class AuthRepoImpl extends AuthRepo{
@@ -17,12 +19,22 @@ class AuthRepoImpl extends AuthRepo{
   }
 
   @override
-  Future loginWithGoogle() {
- 
+  Future loginWithGoogle() async {
 
+ Future<UserCredential> signInWithGoogle() async {
+  // Trigger the authentication flow
+  final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
 
-    throw UnimplementedError();
+  // Obtain the auth details from the request
+  final GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
+
+  // Create a new credential
+  final credential = GoogleAuthProvider.credential(
+    accessToken: googleAuth?.accessToken,
+    idToken: googleAuth?.idToken,
+  );
+
+  // Once signed in, return the UserCredential
+  return await FirebaseAuth.instance.signInWithCredential(credential);
   }
-
-
-}
+  }}
